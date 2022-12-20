d9d150b24b7737f5e8d50394e620112cc01b39eb
alvaro-32 <alvarogustavo.droppelmann@alumnos.ulagos.cl>
2022-12-20 01:18:03 -0300

    Add: style for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,50 @@
+import axios from 'axios';
+import { React } from 'react';
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import {
+    InputGroup, Input,
+    Button, VStack,
+    Box, StackDivider,
+    Center, Flex,
+    Image, Text,
+    InputRightElement
+} from '@chakra-ui/react'
 
 function Login() {
     const [message, setMessage] = useState(null);
     const navigate = useNavigate();
+
+    const PaswordInput = () => {
+        const [show, setShow] = useState(false)
+        const handleClick = () => setShow(!show)
+
+        return (
+            <InputGroup size='md'>
+                <Flex>
+                    <Input
+                        position="absolute"
+                        top="-35px"
+                        right="43px"
+                        pr='4.5rem'
+                        type={show ? 'text' : 'password'}
+                        placeholder='Enter password'
+                        size="lg"
+                        width="600px"
+                    />
+                    <InputRightElement width='3.5rem'>
+                        <Button h='3.09rem' size='lg' onClick={handleClick}
+                            position="absolute"
+                            top="-36px"
+                            left="-82px"
+                            bg="#bec2c8">
+                            {show ? 'Hide' : 'Show'}
+                        </Button>
+                    </InputRightElement>
+                </Flex>
+            </InputGroup>
+        )
+    }
 
     const handlerSubmit = async (event) => {
         try {
@@ -41,9 +81,27 @@ function Login() {
     return (
         <div>
             <form onSubmit={handlerSubmit}>
-                <input type="email" name="email" placeholder="Email" />
-                <input type="password" name="password" placeholder="Password" />
-                <button>Enviar</button>
+
+                <Flex height="100vh" alignItems="center" justifyContent="center">
+                    <Box
+                        boxSize={"700px"} borderColor={"#4d5c7b"}
+                        borderRadius={"lg"} color="#242434" px="0.5" borderWidth="0.5px" borderStyle={"solid"}
+                        paddingTop={"20px"}
+                        backgroundColor={"whitesmoke"}
+                        boxShadow={"dark-lg"}
+                    >
+                        <VStack
+                            spacing={"48px"}
+                        >
+                            <Image src="https://apingweb.com/img/aping.png" size="100%" rounded="1rem" borderRadius={"lg"} shadow="2xl"></Image>
+                            <Text fontSize="lg"><b>Sign In</b></Text>
+                            <Input width="600px" variant='outline' type="email" name="email" placeholder="Email" size="lg" />
+                            <PaswordInput />
+                            <Button width="600px" bg='#f64315' variant='hidden' size="lg" alignItems="center">Ingresar</Button>
+                        </VStack>
+                    </Box>
+                </Flex>
+
             </form>
 
             {message && <p>{message}</p>}
