d5b992701a877b953e73594ed8dcb6cffc4707bf
alvaro-32 <alvarogustavo.droppelmann@alumnos.ulagos.cl>
2022-12-19 22:49:23 -0300

    Add: Routes for home and  login pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
 )
