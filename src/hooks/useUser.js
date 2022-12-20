2578c848150794b1f524665a0c5759163a2e53d6
v1nte <ivinte99@gmail.com>
2022-12-20 00:57:20 -0300

    add: register function in useUser hook

---
+++ src/hooks/useUser.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { axios } from 'axios'
+
+const apiURL = 'https://apingweb.com/api'
+
+const useUser = () => {
+  
+  const [ isLoading, setIsLoading ] = useState(false)
+
+  const register = async(userData) =>{
+
+    // Data validation
+    setIsLoading(true)
+    if (!userData.name) return
+    if (!userData.email) return
+    if (!userData.phone) return
+    if (!userData.password) return
+    if (!userData.passworConfirmation) return
+
+    if (userData.passworConfirmation !== userData.password) return
+
+    const headers = {
+      'Content-Type': 'application/json',
+    }
+    const rawData = JSON.stringify(userData)
+
+    try {
+
+      const res = await axios.post(`${apiURL}/register`, rawData, {headers} )
+      const data = res.status ? res.data : res.statusText
+
+      if (!res.status) return console.error('xd')
+
+      sessionStorage.setItem('token', data.token)
+
+    } catch (err) {
+      console.error(err)
+      
+    }
+
+    setIsLoading(false)
+
+  }
+
+  return{
+    isLoading,
+    register,
+    login,
+    user,
+  }
+}
