10260a69958ab32c8aa644753c4564a6f11ec767
v1nte <ivinte99@gmail.com>
2022-12-20 01:13:22 -0300

    add: login function to useUser hook

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,9 +3,14 @@ import { axios } from 'axios'
 
 const apiURL = 'https://apingweb.com/api'
 
+const headers = {
+  'Content-Type': 'application/json',
+}
+
 const useUser = () => {
   
   const [ isLoading, setIsLoading ] = useState(false)
+
 
   const register = async(userData) =>{
 
@@ -19,9 +24,6 @@ const useUser = () => {
 
     if (userData.passworConfirmation !== userData.password) return
 
-    const headers = {
-      'Content-Type': 'application/json',
-    }
     const rawData = JSON.stringify(userData)
 
     try {
@@ -43,10 +45,34 @@ const useUser = () => {
 
   }
 
+  const login = async (userData) => {
+    setIsLoading(true)
+
+    if (!userData.email) return
+    if (!userData.password) return
+
+    const rawData = JSON.stringify(userData)
+
+    try {
+
+      const res = await axios.post(`${apiURL}/login`, rawData, {headers})
+      const data = res.status ? res.data : res.statusText
+
+      if (!res.status) return console.error('xd')
+
+      sessionStorage.setItem('token', data.token)
+      sessionStorage.setItem('name', data.result.name)
+      
+    } catch (err) {
+      console.error(err)
+      
+    }
+
+  }
+
   return{
     isLoading,
     register,
     login,
-    user,
   }
 }
