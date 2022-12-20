9a6792dcf562d156cdfc6b9095d53e8634fe620e
v1nte <ivinte99@gmail.com>
2022-12-20 00:02:33 -0300

    simple styling for Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,37 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Flex, Box, Spacer } from "@chakra-ui/react"
 
-export default function Navbar() {
-  return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        Site Name
-      </Link>
-      <ul>
-        <CustomLink to="/signin">Sig In</CustomLink>
-        <CustomLink to="/signup">Sig Up</CustomLink>
-      </ul>
-    </nav>
-  )
-}
 
-function CustomLink({ to, children, ...props }) {
+const CustomLink = ({ to, children, ...props }) => {
   const resolvedPath = useResolvedPath(to)
   const isActive = useMatch({ path: resolvedPath.pathname, end: true })
 
   return (
-    <li className={isActive ? "active" : ""}>
+    <li style={{listStyleType: 'none'}} className={isActive ? "active" : ""}>
       <Link to={to} {...props}>
         {children}
       </Link>
     </li>
   )
 }
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <Box bg='#3d5c7b' fontSize='xl' color='white'  p={4}>
+        <Flex direction="row">
+          <Link to="/" className="site-title">
+            Site Name
+          </Link>
+
+          <Spacer />
+          <CustomLink style={{marginRight: '2em'}} to="/signin">Sig In</CustomLink>
+          <CustomLink to="/signup">Sig Up</CustomLink>
+        </Flex>
+      </Box>
+    </nav>
+  )
+}
+
+export default Navbar
+
