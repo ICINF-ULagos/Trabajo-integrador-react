71ac31ece0c0bccbf6a50f3c99b714e2f1801c3d
alvaro-32 <alvarogustavo.droppelmann@alumnos.ulagos.cl>
2022-12-20 22:54:08 -0300

    Fix: styles Add: route for Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import {
-    Route,
-    Routes,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import Login from './pages/Login'
+import Login from "./pages/Login";
 //import Register from './pages/Register'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import './App.css';
+import Home from "./pages/Home";
+import Timeline from "./pages/User/Timeline";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/signin" element={<Login />} />
-                </Routes>
-            </div>
-        </>
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/user/timeline" element={<Timeline />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
