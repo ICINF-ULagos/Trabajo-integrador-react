48de6fa614ca06861d15c32e4acf614d6f3dcbf6
v1nte <ivinte99@gmail.com>
2022-12-21 06:24:12 -0300

    add: getNameById function to useUser

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -10,6 +10,23 @@ const headers = {
 const useUser = () => {
   const [isLoading, setIsLoading] = useState(false);
 
+  const getNameById = async (id) => {
+    setIsLoading(true);
+
+    try {
+      const res = await axios.get(`${apiURL}/user/${id}`);
+      const data = res.status ? res.data : res.statusText;
+      const name = data?.data[0]?.name ?? "Unknown"; // api format
+
+      setIsLoading(false);
+      return name;
+    } catch (err) {
+      console.error(err);
+    }
+
+    setIsLoading(false);
+  };
+
   const register = async (userData) => {
     // Data validation
     setIsLoading(true);
@@ -62,6 +79,7 @@ const useUser = () => {
   };
 
   return {
+    getNameById,
     isLoading,
     register,
     login,
